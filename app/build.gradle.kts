c37cad095449f7984b85a2ab2e9fabd7a95b739c
unghui <lamer0712@gmail.com>
2026-09-13 03:42:00 +0900

    fix: default inline playback to 1.5x

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         minSdk = 30
         targetSdk = 35
         versionCode = 19
-        versionName = "0.4.61"
+        versionName = "0.4.62"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     compileOptions {
