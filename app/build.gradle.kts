564c1a3cdc847604cd2c7464fac04c13c4afc400
unghui <lamer0712@gmail.com>
2026-09-13 02:45:15 +0900

    feat: add fullscreen video seek controls

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         minSdk = 30
         targetSdk = 35
         versionCode = 19
-        versionName = "0.4.49"
+        versionName = "0.4.50"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     compileOptions {
